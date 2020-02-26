64137c0b681f138178454074d0c47176afc1a79d
gajomon <vinicius.gajo@fullengenharia.com.br>
2020-02-26 10:26:08 -0300

    Commitando novidades nos estudos sobre preenchimento de planilhas Excel

--- estudos/estudosXLSX/node-xlsx1/index.js
+++ estudos/estudosXLSX/node-xlsx1/index.js
@@ -3,10 +3,32 @@ const ExcelJS = require('exceljs');
 
 var fileName = `${__dirname}\\teste.xlsx`;
 
+// Lendo arquivos de uma planilha xlsx
 var workBook = new ExcelJS.Workbook();
 workBook.xlsx.readFile(fileName)
     .then(function(workBook) {
-        var sheet = workBook.worksheets[0].getCell('D9').value;
-        console.log(sheet);
+        workBook.worksheets[0].getCell('D24').value = 'Att. CENTRO NORDESTE';
+    })
+    .then(() => {
+        var numAleatorio = Math.round(Math.random()*10e8);
+        console.log(numAleatorio);
+        // Salva a planilha com os dados alterados e com o nome aleatório
+        workBook.xlsx.writeFile(`teste_${numAleatorio}.xlsx`);
+    }) 
+    .then(() => {
+        // Apaga a planilha dos arquivos do servidor após ter sido feito o download pelo usuário
+        // ...
+    })
+    .then(() => {
+        console.log(workBook.worksheets[0].getCell('D24').value);
     })
     .catch(err => console.log(err));
+
+// Escrevendo em uma planlinha xlsx
+/*
+var workbook = createAndFillWorkbook();
+workbook.xlsx.writeFile(filename)
+  .then(function() {
+    // done
+  });
+*/
