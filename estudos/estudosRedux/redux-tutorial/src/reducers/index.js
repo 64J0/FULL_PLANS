77a3f4f772460b2451f68377a29ef10d69c5daf9
64J0 <vinigaio97@gmail.com>
2020-08-20 01:14:34 -0300

    Retirando arquivos sobre GridFS e Redux

--- estudos/estudosRedux/redux-tutorial/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-
-import postsReducer from './postsReducer';
-
-const rootReducer = combineReducers({
-  posts: postsReducer
-});
-
-export default rootReducer;
