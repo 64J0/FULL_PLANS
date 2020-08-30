8721c5328d27711e3020ee10e867128b410c29d8
64J0 <vinigaio97@gmail.com>
2020-08-30 20:44:36 -0300

    Front-end: Definindo o padrao de cores que serao usadas no layout do front-end.

---
+++ frontend/src/styles/colorThemes.js
@@ -0,0 +1,86 @@
+// Referência
+// https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+
+const primary = {
+  900: "#004b7b",
+  800: "#0f679a",
+  700: "#1b77ab",
+  600: "#2988bc",
+  500: "#3596c8",
+  400: "#50a4ce",
+  300: "#6db3d5",
+  200: "#93c9e1",
+  100: "#bbddec",
+  50: "#e4f2f7"
+}
+
+const complementary = {
+  900: "#9a420f",
+  800: "#b34d12",
+  700: "#c15413",
+  600: "#ce5b17",
+  500: "#d8601a",
+  400: "#dd743d",
+  300: "#e28b5f",
+  200: "#eaaa8c",
+  100: "#f2cbb9",
+  50: "#f6e8e5"
+}
+
+const analogous1 = {
+  900: "#035040",
+  800: "#076d5c",
+  700: "#0a7d6b",
+  600: "#0d8d7b",
+  500: "#0f9a87",
+  400: "#2caa99",
+  300: "#51b9ab",
+  200: "#82cdc3",
+  100: "#b3e0da",
+  50: "#e0f3f1"
+}
+
+const analogous2 = {
+  900: "#0f229a",
+  800: "#302da8",
+  700: "#3f33b0",
+  600: "#4e3ab9",
+  500: "#583fbf",
+  400: "#735bc9",
+  300: "#8d78d3",
+  200: "#ae9fdf",
+  100: "#cec5eb",
+  50: "#ece7f7"
+}
+
+const triadic1 = {
+  900: "#420f9a",
+  800: "#6715a6",
+  700: "#7918ae",
+  600: "#8d1cb6",
+  500: "#9c1ebb",
+  400: "#ab43c6",
+  300: "#ba66d0",
+  200: "#ce92de",
+  100: "#e1bdeb",
+  50: "#f3e5f6"
+}
+
+const triadic2 = {
+  900: "#9a0f67",
+  800: "#c01470",
+  700: "#d61774",
+  600: "#ee197a",
+  500: "#ff1a7f",
+  400: "#fe4494",
+  300: "#fe68aa",
+  200: "#fd95c3",
+  100: "#fcbfdb",
+  50: "#fee6f0"
+}
+
+const error = {
+  error: "#b00020"
+}
+
+module.exports = { primary, complementary, analogous1, analogous2, triadic1, triadic2, error };
